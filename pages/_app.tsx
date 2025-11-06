bc52c9b0e0ca2d9b07f5e05cc08d1fc1f663a3a6
guannko <guannko@gmail.com>
2025-11-06 21:37:24 +0200

    Update navigation - add AI Analyzer link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,8 @@ import { NewsletterModalContextProvider, useNewsletterModalContext } from 'conte
 import { NavItems } from 'types';
 
 const navItems: NavItems = [
-  { title: 'Awesome SaaS Features', href: '/features' },
+  { title: 'AI Analyzer', href: '/analyzer' },
+  { title: 'Features', href: '/features' },
   { title: 'Pricing', href: '/pricing' },
   { title: 'Contact', href: '/contact' },
   { title: 'Sign up', href: '/sign-up', outlined: true },
